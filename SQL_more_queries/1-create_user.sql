fe7185189ec7c46fb938278e70295b175f37266a
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-15 23:53:58 +0100

    correctly format quote

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,11 +1,11 @@
 -- create a user 
 
 CREATE USER 
-    IF NOT EXISTS user_0d_1@localhost
-    IDENTIFIED BY user_0d_1_pwd;
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
 
 GRANT ALL PRIVILEGES 
     ON *.* 
-    TO user_0d_1@localhost
-    IDENTIFIED BY user_0d_1_pwd;
+    TO 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
