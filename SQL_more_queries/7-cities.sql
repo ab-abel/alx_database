9693b498e75b79c799e975a6c0cbf7b285f3b9d6
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-16 23:17:16 +0100

    task 7 initial commit

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- create a table for cities
+
+CREATE DATABASE
+    IF NOT EXISTS htbn_0d_usa;
+
+CREATE TABLE
+    IF NOT EXISTS
+    htbn_0d_usa.cities(
+     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+     state_id INT NOT NULL FOREIGN KEY REFERENCES states(id),
+     name VARCHAR(256)
+    );
