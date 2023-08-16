e278e7f44bf8ede519eb843ff885362db0bf09af
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-16 22:51:53 +0100

    correction on create table

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -3,11 +3,10 @@
 CREATE DATABASE
     IF NOT EXISTS hbtn_0d_usa;
 
-USE hbtn_0d_usa;
 
 CREATE TABLE
     IF NOT EXISTS
-    states (
+    hbtn_0d_usa.states (
         id INT UNIQUE AUTO INCREMENT NOT NULL PRIMARY KEY,
         name VARCHAR(256)
     );
