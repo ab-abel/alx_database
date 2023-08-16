42896918513cb39a50b56ca0c12688663341716e
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-16 18:05:04 +0100

    task 1 correction

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -4,7 +4,7 @@ CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT ALL PRIVILEGES
+GRANT ALL PRIVILEGES IF NOT EXISTS
     ON *.*
     TO 'user_0d_1'@'%'
 FLUSH PRIVILEGES;
