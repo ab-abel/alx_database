034bd93dcd0d94613cd0512c4d6cf8b26b25f680
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-17 17:49:05 +0100

    initial commit for task 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -3,9 +3,10 @@
 CREATE DATABASE
     IF NOT EXISTS hbtn_0d_usa;
 
+USE  hbtn_0d_usa;
+
 CREATE TABLE
-    IF NOT EXISTS
-    hbtn_0d_usa.states (
+    IF NOT EXISTS states (
         id INT UNIQUE AUTO INCREMENT NOT NULL PRIMARY KEY,
         name VARCHAR(256) NOT NULL
     );
