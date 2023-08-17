7e1f86047dce56acd10b73bca55ae0f4c4a0fff0
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-17 09:30:25 +0100

    correction on foreign key

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,12 +3,13 @@
 CREATE DATABASE
     IF NOT EXISTS htbn_0d_usa;
 
+USE htbn_0d_usa;
 
 CREATE TABLE
-    IF NOT EXISTS htbn_0d_usa.cities(
+    IF NOT EXISTS cities(
         id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
         state_id INT NOT NULL,
         name VARCHAR(256),
         FOREIGN KEY (state_id)
-        REFERENCES htbn_0d_usa.states(id)
+        REFERENCES states(id)
     );
