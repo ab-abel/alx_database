0785cbab0a877fbe297a43e79f8bef7ba4e70410
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-17 10:01:36 +0100

    task 7 initial commit

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -5,9 +5,9 @@ CREATE DATABASE IF NOT EXISTS htbn_0d_usa;
 USE htbn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS cities(
-        id INT UNIQUE AUTO_INCREMENT NOT NULL,
-        state_id INT NOT NULL,
-        name VARCHAR(256) NOT NULL,
-        PRIMARY KEY (id),
-        FOREIGN KEY (state_id) REFERENCES states(id)
+    id INT UNIQUE AUTO_INCREMENT NOT NULL,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
