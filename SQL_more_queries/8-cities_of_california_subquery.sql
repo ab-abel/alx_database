033a5a33ec4f4e1d4e27490edcc0a60f657556ec
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-17 14:03:01 +0100

    task 8 join table

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,8 @@
 -- list all cities in califonia
 
-SELECT `id`, `name`
-    FROM states 
-    WHERE `state_id` IN 
-        (SELECT `id` FROM states 
-        WHERE `name` = 'California')
-    ORDER by `id`;
+SELECT id, name
+    FROM cities 
+    WHERE state_id IN 
+        (SELECT id FROM states 
+        WHERE name = 'California')
+    ORDER by id;
